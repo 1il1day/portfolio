053b6595cb35b89b4242d6d109ff0537f7e7fe0f
1il1day <jgffdet@nate.com>
2025-02-12 14:32:31 +0900

    리액트 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PcLayout from './components/PcLayout';
+import { useMediaQuery } from 'react-responsive';
+import MobileLayout from './components/MobileLayout';
 
 function App() {
+  const isPc = useMediaQuery({
+    query : "(min-width:769px)"
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isPc ? <PcLayout /> : <MobileLayout />}
+    </>
   );
 }
 
