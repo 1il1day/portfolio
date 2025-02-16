2a93f8445eb7449d3bfaa903d0a68cbe51245a70
1il1day <jgffdet@nate.com>
2025-02-16 22:57:57 +0900

    네비게이션 바 추가

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { theme } from '../../theme';
+import AppIcon from '../widgets/AppIcon';
+
+type Props = {
+  toggleMemo?: () => void;
+  isActive?: boolean;
+}
+
+export default function NavBar({toggleMemo, isActive}: Props) {
+  return (
+    <NavBarContainer>
+      <NavBarInner>
+        <AppIcon 
+          appTitle="홈"
+          url="/" 
+          imgUrl="/images/app_home.svg" 
+        />
+        <AppIcon 
+          appTitle="프로젝트"
+          url="/projects" 
+          imgUrl="/images/app_project.svg" 
+        />
+        <AppIcon 
+          appTitle="이력서"
+          url="https://www.naver.com/" 
+          target="_blank" 
+          imgUrl="/images/app_notion.svg" 
+        />
+        <AppIcon 
+          appTitle="프로필"
+          imgUrl="/images/app_info.svg" 
+          onClick={toggleMemo}
+          isActive={isActive}
+        />
+      </NavBarInner>
+    </NavBarContainer>
+  )
+}
+
+const NavBarContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  left: 50%;
+  bottom: 20px;
+  z-index: 20;
+  transform: translateX(-50%);
+  background-color: rgba(29, 29, 29, 0.3);
+  border-radius: 40px;
+  width: calc(100% - (${theme.mobilePadding} * 2));
+  max-width: 496px;
+  height: 114px;
+  @media (min-width: 769px) {
+    bottom: 30px;
+  }
+`;
+const NavBarInner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 24px;
+  width: 100%;
+  max-width: 391px;
+  margin: auto;
+  padding: 0 35px;
+`;
---
+++ src/components/widgets/AppIcon.tsx
@@ -0,0 +1,83 @@
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+type Props = {
+  appTitle: string;
+  url?: string;
+  target?: string;
+  imgUrl: string;
+  onClick?: () => void;
+  isActive?: boolean;
+}
+
+export default function AppIcon({appTitle, url='', target='_self', imgUrl, onClick, isActive}: Props) {
+  const pathname = useLocation().pathname;
+
+  return (
+    <>
+      {url ?
+        <Link to={url} target={target}>
+          <AppIconWrap className={`${pathname === url ? "active-red" : ""}`}>
+            <AppIconBox imgUrl={imgUrl} />
+            <AppTitle>{appTitle}</AppTitle>
+          </AppIconWrap>
+        </Link>
+        :
+        <AppIconWrap
+          className={`${isActive ? "active-green" : ""}`}
+          onClick={onClick}
+        >
+          <AppIconBox imgUrl={imgUrl} />
+          <AppTitle>{appTitle}</AppTitle>
+        </AppIconWrap>
+      }
+    </>
+  )
+}
+
+const AppIconWrap = styled.div`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 5px;
+  @media (pointer: fine) {
+    cursor: pointer;
+  }
+  &.active-red, &.active-green{
+    &:after{
+      content: "";
+      position: absolute;
+      top: -10px;
+      right: -10px;
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+    }
+    &.active-red{
+      &:after{
+        background-color: #FF3B30;
+      }
+    }
+    &.active-green{
+      &:after{
+        background-color: #5EB652;
+      }
+    }
+  }
+`;
+const AppIconBox = styled.div<{imgUrl?: string;}>`
+  width: 64px;
+  height: 64px;
+  border-radius: 15px;
+  background-image: url(${(props)=>props.imgUrl});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: #fff;
+`;
+const AppTitle = styled.h3`
+  font-size: 14px;
+  text-align: center;
+  color: #fff;
+`;
