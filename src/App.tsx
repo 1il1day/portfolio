6cd50ffde6af5f1400c43e46fbeb899327316ab3
1il1day <jgffdet@nate.com>
2025-02-18 16:32:53 +0900

    테스트 페이지 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import TestPage from './pages/TestPage';
 import ProjectsPage from './pages/ProjectsPage';
 
 const App = () => {
@@ -13,9 +12,6 @@ const App = () => {
         
         {/* ProjectsPage 페이지 */}
         <Route path="/projects" element={<ProjectsPage />} />
-
-        {/* FIXME: TestPage 페이지 */}
-        <Route path="/Test" element={<TestPage />} />
       </Routes>
     </Router>
   );
