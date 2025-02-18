b222f1f9ab5fdc6c81b01f4ba58373a128ce8ad8
1il1day <jgffdet@nate.com>
2025-02-18 19:30:27 +0900

    HashRouter -> BrowserRouter 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         {/* Home 페이지 */}
         <Route path="/" element={<HomePage />} />
